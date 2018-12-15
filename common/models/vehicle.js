bfb61a185a1c29b5f3a53c371d3b3cd2e160aa9c
yacaFx <sergiobritor@gmail.com>
2018-12-15 16:33:55 -0500

    Populate driver on find and findById methods

--- common/models/vehicle.js
+++ common/models/vehicle.js
@@ -1,5 +1,25 @@
 'use strict';
 
-module.exports = function(Vehicle) {
+module.exports = Vehicle => {
+  Vehicle.afterRemote('find', (ctx, vehicle, next) => {
+    const Member = Vehicle.app.models.member;
 
+    vehicle.forEach((v, index) => {
+      Member.findById(v.driverId, (err, member) => {
+        v.driver = member;
+        if (index === vehicle.length - 1) {
+          next();
+        }
+      });
+    });
+  });
+
+  Vehicle.afterRemote('findById', (ctx, vehicle, next) => {
+    const Member = Vehicle.app.models.member;
+
+    Member.findById(vehicle.driverId, (err, member) => {
+      vehicle.driver = member;
+      next();
+    });
+  });
 };

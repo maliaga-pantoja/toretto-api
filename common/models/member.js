cca9aff8056404ee00b73365135c268c187230f0
yacafx <sergiobritor@gmail.com>
2019-01-06 16:35:51 -0500

    Fix relations

--- common/models/member.js
+++ common/models/member.js
@@ -1,203 +1,203 @@
 'use strict';
 
 module.exports = function(Member) {
-  Member.afterRemote('login', (ctx, result, next) => {
-    const RoleMapping = Member.app.models.RoleMapping;
-    const Role = Member.app.models.Role;
-    const Vehicle = Member.app.models.Vehicle;
+    // Member.afterRemote('login', (ctx, result, next) => {
+    //   const RoleMapping = Member.app.models.RoleMapping;
+    //   const Role = Member.app.models.Role;
+    //   const Vehicle = Member.app.models.Vehicle;
 
-    Member.findById(result.userId, (err, member) => {
-      RoleMapping.find(
-        {
-          where: {
-            principalId: result.userId,
-          },
-        },
-        (err, mapping) => {
-          Role.findById(mapping[0].roleId, (err, role) => {
-            Vehicle.find(
-              {
-                where: {
-                  driverId: result.userId,
-                },
-              },
-              (err, vehicle) => {
-                result.role = role.name;
-                result.member = member;
-                result.vehicle = vehicle;
+    //   Member.findById(result.userId, (err, member) => {
+    //     RoleMapping.find(
+    //       {
+    //         where: {
+    //           principalId: result.userId,
+    //         },
+    //       },
+    //       (err, mapping) => {
+    //         Role.findById(mapping[0].roleId, (err, role) => {
+    //           Vehicle.find(
+    //             {
+    //               where: {
+    //                 driverId: result.userId,
+    //               },
+    //             },
+    //             (err, vehicle) => {
+    //               result.role = role.name;
+    //               result.member = member;
+    //               result.vehicle = vehicle;
 
-                next();
-              }
-            );
-          });
-        }
-      );
-    });
-  });
+    //               next();
+    //             }
+    //           );
+    //         });
+    //       }
+    //     );
+    //   });
+    // });
 
-  Member.afterRemote('create', (ctx, member, next) => {
-    const RoleMapping = Member.app.models.RoleMapping;
-    const Role = Member.app.models.Role;
-    console.log(member.role);
-    // Get role id
-    Role.find(
-      {
-        where: {
-          name: member.role,
-        },
-      },
-      (err, role) => {
-        if (err) throw err;
-        // create a new record on rolemapping
-        RoleMapping.create(
-          {
-            principalType: 'USER',
-            principalId: member.id,
-            roleId: role.id,
-          },
-          (err, principal) => {
-            if (err) throw err;
-            next();
-          }
-        );
-      }
-    );
-  });
+    // Member.afterRemote('create', (ctx, member, next) => {
+    //   const RoleMapping = Member.app.models.RoleMapping;
+    //   const Role = Member.app.models.Role;
+    //   console.log(member.role);
+    //   // Get role id
+    //   Role.find(
+    //     {
+    //       where: {
+    //         name: member.role,
+    //       },
+    //     },
+    //     (err, role) => {
+    //       if (err) throw err;
+    //       // create a new record on rolemapping
+    //       RoleMapping.create(
+    //         {
+    //           principalType: 'USER',
+    //           principalId: member.id,
+    //           roleId: role.id,
+    //         },
+    //         (err, principal) => {
+    //           if (err) throw err;
+    //           next();
+    //         }
+    //       );
+    //     }
+    //   );
+    // });
 
-  Member.getRides = (id, cb) => {
-    const Rides = Member.app.models.ride;
-    const Company = Member.app.models.company;
-    const Destiny = Member.app.models.destiny;
+    // Member.getRides = (id, cb) => {
+    //   const Rides = Member.app.models.ride;
+    //   const Company = Member.app.models.company;
+    //   const Destiny = Member.app.models.destiny;
 
-    Member.findById(id, (err, member) => {
-      Rides.find(
-        {
-          where: {
-            driverId: id,
-          },
-        },
-        (err, rides) => {
-          const companyIds = rides.map(ride => ride.companyId);
-          const passengerIds = rides.map(ride => ride.passengerId);
-          let destinyIds = [];
-          rides
-            .map(ride => ride.destinyIds)
-            .forEach(destiny => {
-              destiny.forEach(item => {
-                if (!destinyIds.includes(item)) {
-                  destinyIds.push(item);
-                }
-              });
-            });
+    //   Member.findById(id, (err, member) => {
+    //     Rides.find(
+    //       {
+    //         where: {
+    //           driverId: id,
+    //         },
+    //       },
+    //       (err, rides) => {
+    //         const companyIds = rides.map(ride => ride.companyId);
+    //         const passengerIds = rides.map(ride => ride.passengerId);
+    //         let destinyIds = [];
+    //         rides
+    //           .map(ride => ride.destinyIds)
+    //           .forEach(destiny => {
+    //             destiny.forEach(item => {
+    //               if (!destinyIds.includes(item)) {
+    //                 destinyIds.push(item);
+    //               }
+    //             });
+    //           });
 
-          Company.find(
-            {
-              where: {
-                id: {
-                  inq: companyIds,
-                },
-              },
-            },
-            (err, companies) => {
-              const companiesFound = companies;
-              Member.find(
-                {
-                  where: {
-                    id: {
-                      inq: passengerIds,
-                    },
-                  },
-                },
-                (err, passengers) => {
-                  const passengersFound = passengers;
-                  Destiny.find(
-                    {
-                      where: {
-                        id: {
-                          inq: destinyIds,
-                        },
-                      },
-                    },
-                    (err, destinies) => {
-                      const destiniesFound = destinies;
+    //         Company.find(
+    //           {
+    //             where: {
+    //               id: {
+    //                 inq: companyIds,
+    //               },
+    //             },
+    //           },
+    //           (err, companies) => {
+    //             const companiesFound = companies;
+    //             Member.find(
+    //               {
+    //                 where: {
+    //                   id: {
+    //                     inq: passengerIds,
+    //                   },
+    //                 },
+    //               },
+    //               (err, passengers) => {
+    //                 const passengersFound = passengers;
+    //                 Destiny.find(
+    //                   {
+    //                     where: {
+    //                       id: {
+    //                         inq: destinyIds,
+    //                       },
+    //                     },
+    //                   },
+    //                   (err, destinies) => {
+    //                     const destiniesFound = destinies;
 
-                      const fullRides = [];
+    //                     const fullRides = [];
 
-                      rides.forEach(ride => {
-                        ride.company = companiesFound.filter(company => {
-                          if (String(company.id) === ride.companyId) {
-                            return company;
-                          }
-                        });
+    //                     rides.forEach(ride => {
+    //                       ride.company = companiesFound.filter(company => {
+    //                         if (String(company.id) === ride.companyId) {
+    //                           return company;
+    //                         }
+    //                       });
 
-                        ride.passenger = passengersFound.filter(passenger => {
-                          if (String(passenger.id) === ride.passengerId) {
-                            return passenger;
-                          }
-                        });
+    //                       ride.passenger = passengersFound.filter(passenger => {
+    //                         if (String(passenger.id) === ride.passengerId) {
+    //                           return passenger;
+    //                         }
+    //                       });
 
-                        const currentDestinies = [];
+    //                       const currentDestinies = [];
 
-                        destiniesFound.forEach(destiny => {
-                          ride.destinyIds.forEach(item => {
-                            if (String(item) === String(destiny.id)) {
-                              currentDestinies.push(destiny);
-                            }
-                          });
-                        });
+    //                       destiniesFound.forEach(destiny => {
+    //                         ride.destinyIds.forEach(item => {
+    //                           if (String(item) === String(destiny.id)) {
+    //                             currentDestinies.push(destiny);
+    //                           }
+    //                         });
+    //                       });
 
-                        const newRide = {
-                          companies: ride.company,
-                          destinies: currentDestinies,
-                          driver: member,
-                          passenger: ride.passenger,
-                          route: ride.route,
-                          type: ride.type,
-                        };
+    //                       const newRide = {
+    //                         companies: ride.company,
+    //                         destinies: currentDestinies,
+    //                         driver: member,
+    //                         passenger: ride.passenger,
+    //                         route: ride.route,
+    //                         type: ride.type,
+    //                       };
 
-                        fullRides.push(newRide);
-                      });
-                      cb(null, fullRides);
-                    }
-                  );
-                }
-              );
-            }
-          );
-        }
-      );
-    });
-  };
+    //                       fullRides.push(newRide);
+    //                     });
+    //                     cb(null, fullRides);
+    //                   }
+    //                 );
+    //               }
+    //             );
+    //           }
+    //         );
+    //       }
+    //     );
+    //   });
+    // };
 
-  Member.remoteMethod('getRides', {
-    description: 'Get driver rides.',
-    http: {
-      path: '/:id/rides',
-      verb: 'get',
-    },
-    accepts: {
-      arg: 'id',
-      type: 'string',
-    },
-    returns: {
-      arg: 'rides',
-      type: 'array',
-    },
-  });
+    // Member.remoteMethod('getRides', {
+    //   description: 'Get driver rides.',
+    //   http: {
+    //     path: '/:id/rides',
+    //     verb: 'get',
+    //   },
+    //   accepts: {
+    //     arg: 'id',
+    //     type: 'string',
+    //   },
+    //   returns: {
+    //     arg: 'rides',
+    //     type: 'array',
+    //   },
+    // });
 
-  Member.remoteMethod('getMembersByRole', {
-    description: 'Get members by role.',
-    http: {
-      path: '/role/:id',
-      verb: 'get',
-    },
-    accepts: {
-      arg: 'id',
-      type: 'string',
-    },
-    returns: {
-      arg: 'rides',
-      type: 'array',
-    },
-  });
+    // Member.remoteMethod('getMembersByRole', {
+    //   description: 'Get members by role.',
+    //   http: {
+    //     path: '/role/:id',
+    //     verb: 'get',
+    //   },
+    //   accepts: {
+    //     arg: 'id',
+    //     type: 'string',
+    //   },
+    //   returns: {
+    //     arg: 'rides',
+    //     type: 'array',
+    //   },
+    // });
 };
--- server/boot/script.js
+++ server/boot/script.js
@@ -5,165 +5,211 @@
 //   var Company = app.models.company;
 //   var Role = app.models.Role;
 //   var RoleMapping = app.models.RoleMapping;
+//   var Vehicle = app.models.vehicle;
 
 //   Company.create(
-//     [
-//       {name: 'Compañia 1'},
-//       {name: 'Compañia 2'},
-//       {name: 'Compañia 3'},
-//       {name: 'Compañia 4'},
-//     ],
-//     (err, companies) => {
-//       Member.create(
-//         [
-//           {
-//             companyId: companies[0].id,
-//             username: 'Giacomo',
-//             name: 'Giacomo',
-//             lastname: 'Giacomo',
-//             email: 'admin@admin.com',
-//             password: '123123a',
-//           },
-//           {
-//             companyId: companies[0].id,
-//             username: 'Hugo',
-//             name: 'Hugo',
-//             lastname: 'Hugo',
-//             email: 'contacto_admin@company.com',
-//             password: '123123a',
-//           },
-//           {
-//             companyId: companies[0].id,
-//             username: 'Pedro',
-//             name: 'Pedro',
-//             lastname: 'Pedro',
-//             email: 'contacto@seguridad.com',
-//             password: '123123a',
-//           },
-//           {
-//             companyId: companies[0].id,
-//             username: 'Paco',
-//             name: 'Paco',
-//             lastname: 'Paco',
-//             email: 'chofer@company.com',
-//             password: '123123a',
-//           },
-//           {
-//             companyId: companies[0].id,
-//             username: 'Luis',
-//             name: 'Luis',
-//             lastname: 'Luis',
-//             email: 'pasajero@hospital.com',
-//             password: '123123a',
-//           },
-//         ],
-//         (err, members) => {
-//           if (err) throw err;
-
-//           // create the admin role
-//           Role.create(
-//             {
-//               name: 'admin',
-//             },
-//             (err, role) => {
-//               if (err) throw err;
-
-//               // make Giacomo an admin
-//               role.principals.create(
-//                 {
-//                   principalType: RoleMapping.USER,
-//                   principalId: members[0].id,
+//       [{
+//           name: 'Compañia 1',
+//         },
+//         {
+//           name: 'Compañia 2',
+//         },
+//         {
+//           name: 'Compañia 3',
+//         },
+//         {
+//           name: 'Compañia 4',
+//         },
+//       ],
+//         (err, companies) => {
+//           Member.create(
+//               [{
+//                   companyId: companies[0].id,
+//                   username: 'Giacomo',
+//                   name: 'Giacomo',
+//                   lastname: 'Giacomo',
+//                   email: 'admin@admin.com',
+//                   password: '123123a',
 //                 },
-//                 (err, principal) => {
-//                   if (err) throw err;
-//                 }
-//               );
-//             }
-//           );
-
-//           // create the contact_admin role
-//           Role.create(
-//             {
-//               name: 'contact_admin',
-//             },
-//             (err, role) => {
-//               if (err) throw err;
-
-//               // make Giacomo an admin
-//               role.principals.create(
 //                 {
-//                   principalType: RoleMapping.USER,
-//                   principalId: members[1].id,
+//                   companyId: companies[0].id,
+//                   username: 'Hugo',
+//                   name: 'Hugo',
+//                   lastname: 'Hugo',
+//                   email: 'contacto_admin@company.com',
+//                   password: '123123a',
 //                 },
-//                 (err, principal) => {
-//                   if (err) throw err;
-//                 }
-//               );
-//             }
-//           );
-//           // create the contact role
-//           Role.create(
-//             {
-//               name: 'contact',
-//             },
-//             (err, role) => {
-//               if (err) throw err;
-
-//               // make Giacomo an admin
-//               role.principals.create(
 //                 {
-//                   principalType: RoleMapping.USER,
-//                   principalId: members[2].id,
+//                   companyId: companies[0].id,
+//                   username: 'Pedro',
+//                   name: 'Pedro',
+//                   lastname: 'Pedro',
+//                   email: 'contacto@seguridad.com',
+//                   password: '123123a',
 //                 },
-//                 (err, principal) => {
-//                   if (err) throw err;
-//                 }
-//               );
-//             }
-//           );
-//           // create the driver role
-//           Role.create(
-//             {
-//               name: 'driver',
-//             },
-//             (err, role) => {
-//               if (err) throw err;
-
-//               // make Giacomo an admin
-//               role.principals.create(
 //                 {
-//                   principalType: RoleMapping.USER,
-//                   principalId: members[3].id,
+//                   companyId: companies[0].id,
+//                   username: 'Paco',
+//                   name: 'Paco',
+//                   lastname: 'Paco',
+//                   email: 'chofer@company.com',
+//                   password: '123123a',
 //                 },
-//                 (err, principal) => {
-//                   if (err) throw err;
-//                 }
-//               );
-//             }
-//           );
-//           // create the passenger role
-//           Role.create(
-//             {
-//               name: 'passenger',
-//             },
-//             (err, role) => {
-//               if (err) throw err;
-
-//               // make Giacomo an admin
-//               role.principals.create(
 //                 {
-//                   principalType: RoleMapping.USER,
-//                   principalId: members[4].id,
+//                   companyId: companies[0].id,
+//                   username: 'Luis',
+//                   name: 'Luis',
+//                   lastname: 'Luis',
+//                   email: 'pasajero@hospital.com',
+//                   password: '123123a',
 //                 },
-//                 (err, principal) => {
+//               ],
+//                 (err, members) => {
 //                   if (err) throw err;
+
+//                     // create the admin role
+//                   Role.create({
+//                       name: 'admin',
+//                     },
+//                         (err, role) => {
+//                           if (err) throw err;
+
+//                             // make Giacomo an admin
+//                           role.principals.create({
+//                               principalType: RoleMapping.USER,
+//                               principalId: members[0].id,
+//                             },
+//                                 (err, principal) => {
+//                                   if (err) throw err;
+//                                 }
+//                             );
+//                         }
+//                     );
+
+//                     // create the contact_admin role
+//                   Role.create({
+//                       name: 'contact_admin',
+//                     },
+//                         (err, role) => {
+//                           if (err) throw err;
+
+//                             // make Giacomo an admin
+//                           role.principals.create({
+//                               principalType: RoleMapping.USER,
+//                               principalId: members[1].id,
+//                             },
+//                                 (err, principal) => {
+//                                   if (err) throw err;
+//                                 }
+//                             );
+//                         }
+//                     );
+//                     // create the contact role
+//                   Role.create({
+//                       name: 'contact',
+//                     },
+//                         (err, role) => {
+//                           if (err) throw err;
+
+//                             // make Giacomo an admin
+//                           role.principals.create({
+//                               principalType: RoleMapping.USER,
+//                               principalId: members[2].id,
+//                             },
+//                                 (err, principal) => {
+//                                   if (err) throw err;
+//                                 }
+//                             );
+//                         }
+//                     );
+//                     // create the driver role
+//                   Role.create({
+//                       name: 'driver',
+//                     },
+//                         (err, role) => {
+//                           if (err) throw err;
+
+//                             // make Giacomo an admin
+//                           role.principals.create({
+//                               principalType: RoleMapping.USER,
+//                               principalId: members[3].id,
+//                             },
+//                                 (err, principal) => {
+//                                   if (err) throw err;
+//                                 }
+//                             );
+//                         }
+//                     );
+//                     // create the passenger role
+//                   Role.create({
+//                       name: 'passenger',
+//                     },
+//                         (err, role) => {
+//                           if (err) throw err;
+
+//                             // make Giacomo an admin
+//                           role.principals.create({
+//                               principalType: RoleMapping.USER,
+//                               principalId: members[4].id,
+//                             },
+//                                 (err, principal) => {
+//                                   if (err) throw err;
+//                                 }
+//                             );
+//                         }
+//                     );
+
+//                   Vehicle.create({
+//                       color: 'Gris',
+//                       created_at: new Date(),
+//                       driverId: members[3].id,
+//                       km: '90000',
+//                       model: 'Sentra',
+//                       numberChasis: '981273',
+//                       numberMotor: '812637',
+//                       plate: 'ABC-123',
+//                       soat: '761523761',
+//                       soatName: 'Rimac',
+//                       status: true,
+//                       tradeMark: 'Nissan',
+//                       type: 'Coupé',
+//                       year: '2015',
+//                     });
+
+//                   Vehicle.create({
+//                       color: 'Negro',
+//                       created_at: new Date(),
+//                       driverId: members[3].id,
+//                       km: '90000',
+//                       model: 'Yaris',
+//                       numberChasis: '951273',
+//                       numberMotor: '813637',
+//                       plate: 'ABD-123',
+//                       soat: '761553761',
+//                       soatName: 'Rimac',
+//                       status: true,
+//                       tradeMark: 'Toyota',
+//                       type: 'Coupé',
+//                       year: '2014',
+//                     });
+//                   Vehicle.create({
+//                       color: 'Rojo',
+//                       created_at: new Date(),
+//                       driverId: members[3].id,
+//                       km: '90000',
+//                       model: 'Model S',
+//                       numberChasis: '951273',
+//                       numberMotor: '813637',
+//                       plate: 'ABD-123',
+//                       soat: '761553761',
+//                       soatName: 'Rimac',
+//                       status: true,
+//                       tradeMark: 'Tesla',
+//                       type: 'Coupé',
+//                       year: '2016',
+//                     });
 //                 }
-//               );
-//             }
-//           );
+//             );
 //         }
-//       );
-//     }
-//   );
+//     );
 // };
-

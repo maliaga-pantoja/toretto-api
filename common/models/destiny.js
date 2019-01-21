c61abc1a1d4ded80791d66b4b09917571b4fe75f
yacafx <sergiobritor@gmail.com>
2019-01-20 20:04:14 -0500

    Add company relation

--- common/models/destiny.js
+++ common/models/destiny.js
@@ -1,25 +1,25 @@
 'use strict';
 
 module.exports = Destiny => {
-  Destiny.afterRemote('find', (ctx, destiny, next) => {
-    const Company = Destiny.app.models.company;
+    // Destiny.afterRemote('find', (ctx, destiny, next) => {
+    //   const Company = Destiny.app.models.company;
 
-    destiny.forEach((v, index) => {
-      Company.findById(v.companyId, (err, company) => {
-        v.company = company;
-        if (index === destiny.length - 1) {
-          next();
-        }
-      });
-    });
-  });
+    //   destiny.forEach((v, index) => {
+    //     Company.findById(v.companyId, (err, company) => {
+    //       v.company = company;
+    //       if (index === destiny.length - 1) {
+    //         next();
+    //       }
+    //     });
+    //   });
+    // });
 
-  Destiny.afterRemote('findById', (ctx, destiny, next) => {
-    const Company = Destiny.app.models.company;
+    // Destiny.afterRemote('findById', (ctx, destiny, next) => {
+    //   const Company = Destiny.app.models.company;
 
-    Company.findById(destiny.companyId, (err, company) => {
-      destiny.company = company;
-      next();
-    });
-  });
+    //   Company.findById(destiny.companyId, (err, company) => {
+    //     destiny.company = company;
+    //     next();
+    //   });
+    // });
 };

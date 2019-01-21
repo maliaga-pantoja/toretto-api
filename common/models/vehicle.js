779a8ed5c0baeb2f02c7930a8c1c86e55f6ea9aa
yacafx <sergiobritor@gmail.com>
2019-01-20 19:39:32 -0500

    Fix vehicle - driver relation

--- common/models/vehicle.js
+++ common/models/vehicle.js
@@ -1,25 +1,25 @@
 'use strict';
 
 module.exports = Vehicle => {
-  Vehicle.afterRemote('find', (ctx, vehicle, next) => {
-    const Member = Vehicle.app.models.member;
+    // Vehicle.afterRemote('find', (ctx, vehicle, next) => {
+    //     const Member = Vehicle.app.models.member;
 
-    vehicle.forEach((v, index) => {
-      Member.findById(v.driverId, (err, member) => {
-        v.driver = member;
-        if (index === vehicle.length - 1) {
-          next();
-        }
-      });
-    });
-  });
+    //     vehicle.forEach((v, index) => {
+    //         Member.findById(v.driverId, (err, member) => {
+    //             v.driver = member;
+    //             if (index === vehicle.length - 1) {
+    //                 next();
+    //             }
+    //         });
+    //     });
+    // });
 
-  Vehicle.afterRemote('findById', (ctx, vehicle, next) => {
-    const Member = Vehicle.app.models.member;
+    // Vehicle.afterRemote('findById', (ctx, vehicle, next) => {
+    //   const Member = Vehicle.app.models.member;
 
-    Member.findById(vehicle.driverId, (err, member) => {
-      vehicle.driver = member;
-      next();
-    });
-  });
+    //   Member.findById(vehicle.driverId, (err, member) => {
+    //     vehicle.driver = member;
+    //     next();
+    //   });
+    // });
 };

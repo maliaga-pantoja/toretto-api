7c021a15ad7790cb3ac7bd0f7724a4b290afd7ee
yacafx <sergiobritor@gmail.com>
2019-01-22 23:17:16 -0500

    Add ride relations

--- common/models/ride.js
+++ common/models/ride.js
@@ -1,80 +1,79 @@
 'use strict';
 
 module.exports = Ride => {
-  Ride.afterRemote('find', (ctx, rides, next) => {
-    const Member = Ride.app.models.member;
-    const Company = Ride.app.models.company;
-    const Destiny = Ride.app.models.destiny;
+    // Ride.afterRemote('find', (ctx, rides, next) => {
+    //     const Member = Ride.app.models.member;
+    //     const Company = Ride.app.models.company;
+    //     const Destiny = Ride.app.models.destiny;
 
-    rides.forEach((ride, index) => {
-      Member.find({}, (err, members) => {
-        Company.findById(ride.companyId, (err, company) => {
-          Destiny.find(
-            {
-              where: {
-                id: {
-                  inq: ride.destinyIds,
-                },
-              },
-            },
-            (err, destinies) => {
-              ride.driver = members.find(
-                member => String(member.id) === ride.driverId
-              );
+    //     rides.forEach((ride, index) => {
+    //         Member.find({}, (err, members) => {
+    //             Company.findById(ride.companyId, (err, company) => {
+    //                 Destiny.find({
+    //                         where: {
+    //                             id: {
+    //                                 inq: ride.destinyIds,
+    //                             },
+    //                         },
+    //                     },
+    //                     (err, destinies) => {
+    //                         console.log(destinies.length)
+    //                         ride.driver = members.find(
+    //                             member => String(member.id) === ride.driverId
+    //                         );
 
-              ride.passenger = members.find(
-                member => String(member.id) === ride.passengerId
-              );
+    //                         ride.passenger = members.find(
+    //                             member => String(member.id) === ride.passengerId
+    //                         );
 
-              ride.company = company;
+    //                         ride.company = company;
 
-              const destiniesFound = [];
-              destinies.forEach(destiny => {
-                destiniesFound.push(destiny);
-              });
+    //                         const destiniesFound = [];
+    //                         destinies.forEach(destiny => {
+    //                             destiniesFound.push(destiny);
+    //                         });
 
-              ride.destinies = destiniesFound;
+    //                         ride.destinies = destiniesFound;
 
-              if (index === rides.length - 1) {
-                next();
-              }
-            }
-          );
-        });
-      });
-    });
-  });
+    //                         if (index === rides.length - 1) {
+    //                             next();
+    //                         }
+    //                     }
+    //                 );
+    //             });
+    //         });
+    //     });
+    // });
 
-  Ride.afterRemote('findById', (ctx, ride, next) => {
-    const Member = Ride.app.models.member;
-    const Company = Ride.app.models.company;
-    const Destiny = Ride.app.models.destiny;
+    // Ride.afterRemote('findById', (ctx, ride, next) => {
+    //     const Member = Ride.app.models.member;
+    //     const Company = Ride.app.models.company;
+    //     const Destiny = Ride.app.models.destiny;
 
-    Member.findById(ride.driverId, (err, driver) => {
-      Member.findById(ride.passengerId, (err, passenger) => {
-        Company.findById(ride.companyId, (err, company) => {
-          Destiny.find(
-            {
-              where: {
-                id: {
-                  inq: ride.destinyIds,
-                },
-              },
-            },
-            (err, destinies) => {
-              const destiniesFound = [];
-              destinies.forEach(destiny => {
-                destiniesFound.push(destiny);
-              });
-              ride.driver = driver;
-              ride.company = company;
-              ride.passenger = passenger;
-              ride.destinies = destiniesFound;
-              next();
-            }
-          );
-        });
-      });
-    });
-  });
+    //     Member.findById(ride.driverId, (err, driver) => {
+    //         Member.findById(ride.passengerId, (err, passenger) => {
+    //             Company.findById(ride.companyId, (err, company) => {
+    //                 Destiny.find({
+    //                         where: {
+    //                             id: {
+    //                                 inq: ride.destinyIds,
+    //                             },
+    //                         },
+    //                     },
+    //                     (err, destinies) => {
+    //                         const destiniesFound = [];
+    //                         destinies.forEach(destiny => {
+    //                             destiniesFound.push(destiny);
+    //                         });
+    //                         ride.driver = driver;
+    //                         ride.company = company;
+    //                         ride.passenger = passenger;
+    //                         ride.destinies = destiniesFound;
+    //                         next();
+    //                     }
+    //                 );
+    //             });
+    //         });
+    //     });
+    // });
 };

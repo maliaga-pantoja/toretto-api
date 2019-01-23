872118e77c87114ccd7843b25192a4bfaa598a81
yacafx <sergiobritor@gmail.com>
2019-01-22 22:39:09 -0500

    Fix getZones service

--- common/models/company.js
+++ common/models/company.js
@@ -1,41 +1,37 @@
 'use strict';
 
 module.exports = Company => {
-  Company.getZones = (id, cb) => {
-    const Destiny = Company.app.models.destiny;
+    Company.getZones = (id, cb) => {
+        const Destiny = Company.app.models.destiny;
+        console.log(id)
+        Destiny.find({},
+            (err, destinies) => {
+                destinies = destinies.filter(d => d.companyId === id);
+                const zones = [];
+                console.log(destinies.length)
+                destinies.forEach(destiny => {
+                    if (!zones.includes(destiny.zone)) {
+                        zones.push(destiny.zone);
+                    }
+                });
+                cb(null, zones);
+            }
+        );
+    };
 
-    Destiny.find(
-      {
-        where: {
-          companyId: id,
+    Company.remoteMethod('getZones', {
+        description: 'Get company zones',
+        http: {
+            path: '/:id/zones',
+            verb: 'get',
         },
-      },
-      (err, destinies) => {
-        const zones = [];
-
-        destinies.forEach(destiny => {
-          if (!zones.includes(destiny.zone)) {
-            zones.push(destiny.zone);
-          }
-        });
-        cb(null, zones);
-      }
-    );
-  };
-
-  Company.remoteMethod('getZones', {
-    description: 'Get company zones',
-    http: {
-      path: '/:id/zones',
-      verb: 'get',
-    },
-    accepts: {
-      arg: 'id',
-      type: 'string',
-    },
-    returns: {
-      arg: 'zones',
-      type: 'array',
-    },
-  });
+        accepts: {
+            arg: 'id',
+            type: 'string',
+        },
+        returns: {
+            arg: 'zones',
+            type: 'array',
+        },
+    });
 };

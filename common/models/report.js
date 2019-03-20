aa4c400f625ce1c3bdeb48fb08268270db5c4452
yacafx <sergiobritor@gmail.com>
2019-03-20 17:51:59 -0400

    Get total destinies from today

--- common/models/report.js
+++ common/models/report.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = function(Report) {
-
     //** Get current day rides
     async function getTodayRides(companyId) {
         try {
@@ -11,12 +10,13 @@ module.exports = function(Report) {
                 where: {
                     date: new Date()
                 }
-            })
+            });
 
-            rides = companyId ? rides.filter(r => String(r.companyId) === companyId) : rides;
+            rides = companyId ?
+                rides.filter(r => String(r.companyId) === companyId) :
+                rides;
 
             return rides.length;
-
         } catch (e) {
             return e.message;
         }
@@ -27,23 +27,22 @@ module.exports = function(Report) {
         try {
             const Rides = Report.app.models.ride;
 
-
             const rides = await Rides.find({
                 where: {
                     and: [{
-                        date: new Date()
-                    }, {
-                        status: "2"
-                    }]
+                            date: new Date()
+                        },
+                        {
+                            status: '2'
+                        }
+                    ]
                 }
-            })
+            });
             return rides.length;
-
         } catch (e) {
             return e.message;
         }
     }
-
 
     //** Get tomorrow rides
     async function getTomorrowRides(companyId) {
@@ -58,18 +57,31 @@ module.exports = function(Report) {
                 where: {
                     date: tomorrow
                 }
-            })
+            });
             return rides.length;
+        } catch (e) {
+            return e.message;
+        }
+    }
+
+    //** Get total destinies from today  */
+    async function getTotalTodayDestinies(companyId) {
+        try {
+            const todayRides = await getTodayRides(companyId);
+            let totalDestinies = 0;
+
+            todayRides.forEach(ride => {
+                totalDestinies += ride.destinyIds.length;
+            });
+
+            return totalDestinies;
 
         } catch (e) {
             return e.message;
         }
     }
 
-
-
-    Report.getDashboard = async (companyId) => {
-
+    Report.getDashboard = async companyId => {
         return {
             rides: {
                 today: await getTodayRides(companyId),
@@ -85,31 +97,31 @@ module.exports = function(Report) {
                 base: 0,
                 additional: 0,
                 baseTomorrow: 0,
-                additionalTomorrow: 0,
+                additionalTomorrow: 0
             },
             passengers: {
                 today: 0,
                 onBoard: 0,
                 tomorrow: 0
             }
-        }
-    }
+        };
+    };
 
     Report.remoteMethod('getDashboard', {
         description: 'Get dashboard',
         http: {
             path: '/dashboard',
-            verb: 'get',
+            verb: 'get'
         },
         accepts: {
             arg: 'companyId',
             type: 'string',
             required: false,
-            description: "CompanyId Id. If not set, get all data."
+            description: 'CompanyId Id. If not set, get all data.'
         },
         returns: {
             root: true,
             type: 'object'
-        },
+        }
     });
 };
